58c7f4329765be7bfd2882bd621e432ba4cd2bda
Mikhail Podelyshev <icyfresh@mail.ru>
2025-05-07 00:46:37 +0300

    module48_HW_modified

--- index.php
+++ index.php
@@ -6,6 +6,11 @@ interface SpecialFunction {
     public function useSpecialFunction():string;
 }
 
+interface OnlyForCars {
+    public function cleanWindows():string;
+    public function beepBeep():string;
+}
+
 //Абстрактный класс для всех машин
 
 abstract class Machine {
@@ -25,7 +30,7 @@ abstract class Machine {
 
 //Класс для легкового автомобиля
 
-class Car extends Machine implements SpecialFunction {
+class Car extends Machine implements SpecialFunction, OnlyForCars {
     public function __construct(string $name){
         parent::__construct($name);
     }
@@ -40,6 +45,14 @@ class Car extends Machine implements SpecialFunction {
 
     public function useSpecialFunction():string{
         return "{$this->name} использует закись азота!";
+    }
+
+    public function cleanWindows():string{
+        return "{$this->name} умеет включать дворники!";
+    }
+
+    public function beepBeep():string{
+        return "{$this->name} умеет сигналить!";
     }
 }
 
@@ -65,7 +78,7 @@ class Bulldozer extends Machine implements SpecialFunction {
 
 //Класс для танка
 
-class Tank extends Machine {
+class Tank extends Machine implements SpecialFunction {
     public function __construct(string $name){
         parent::__construct($name);
     }
@@ -76,6 +89,10 @@ class Tank extends Machine {
 
     public function driveBackward():string{
         return "{$this->name} едет назад на гусеницах";
+    }
+
+    public function useSpecialFunction():string{
+        return "{$this->name} умеет стрелять!";
     }
 }
 
@@ -91,6 +108,17 @@ function operateMachine(Machine $machine){
 
     if ($machine instanceof SpecialFunction){
         echo $machine->useSpecialFunction();
+        echo "\n";
+    }
+
+    if ($machine instanceof OnlyForCars){
+        echo $machine->beepBeep();
+        echo "\n";
+        echo $machine->cleanWindows();
+        echo "\n";
+    } else {
+        echo "{$machine->getName()} не умеет ни сигналить, ни включать дворники \xF0\x9F\x98\xA2";
+        echo "\n";
     }
 
 }
@@ -100,7 +128,7 @@ $bulldozer = new Bulldozer("Бульдозер CAT");
 $tank = new Tank("Т-90");
 
 echo "<pre>";
-print_r($car->getName());
+print_r(operateMachine($car));
 echo "\n";
 print_r(operateMachine($bulldozer));
 echo "\n";
